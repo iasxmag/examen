898b8ad253b63a47a4491920258bdaefb9169f6f
iasxmag <maghiariasmina@gmail.com>
2026-05-13 13:11:15 +0200

    ruta listar

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,4 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//para guardar
 Route::post('/personajes', [App\Http\Controllers\PersonajeController::class, 'store']);
+//para listar
+Route::get('/personajes', [App\Http\Controllers\PersonajeController::class, 'index']);
