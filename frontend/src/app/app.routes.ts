fe975bc841995a782e2c7c4c27194d12146f1682
iasxmag <maghiariasmina@gmail.com>
2026-05-13 13:06:34 +0200

    configuracion

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import{ Routes } from '@angular/router';
 import { MarioDexPageComponent } from './features/mario/mariodex-page/mariodex-page.component';
 
 export const routes: Routes = [
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://backend-yx9j.onrender.com/api', 
+};
